06cde8ae98630a9b6d53ed30d4afee9653333a60
literate <gaekoon@gmail.com>
2024-01-30 20:56:36 +0900

    Feat: HttpBasic 인증
    - base64로 username과 password를 인코딩한다
    - 반드시 TLS을 통해야 한다
    - 커스텀 클래스는 AuthenticationEntryPoint의 구현체여야 한다

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,16 +11,12 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        // 둘다 설정해놓으면 formLogin
-        http.formLogin();
-        http.httpBasic();
-//         Custom authentication entry point가 우선 순위가 가장 높다
-//        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
-//            @Override
-//            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-//                System.out.println("custom entryPoint");
-//            }
-//        });
+        http.httpBasic().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+        // 세션을 사용하지 않으면 매 요청 때마다 인증 과정을 거치게 된다
+        // Context를 저장하지도 않는다
+        // existingAuth == null
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
